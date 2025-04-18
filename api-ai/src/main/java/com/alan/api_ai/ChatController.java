f46f8581cdc099bd554cbef49ab2213f6f873f30
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-04-18 03:32:46 -0300

    feat: Implemented a new method using the "OpenAI" API and created a new environment variable

--- api-ai/src/main/java/com/alan/api_ai/ChatController.java
+++ api-ai/src/main/java/com/alan/api_ai/ChatController.java
@@ -1,6 +1,7 @@
 package com.alan.api_ai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,13 @@ public class ChatController {
 
     public ChatController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
     }
 }
